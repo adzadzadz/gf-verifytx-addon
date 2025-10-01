8368f017dca377e40268a0555875e121890c988c
Adrian T. Saycon <adzbite@gmail.com>
2025-10-02 00:12:53 +0800

    Fix CI/CD pipeline issues
    - Add WP_Error class mock to test bootstrap
    - Fix GitHub Actions workflow syntax for conditional steps
    - Remove problematic security scan job
    - Add fallback for missing .distignore file
    - Define missing plugin constants for tests
    - Fix hashFiles conditional expressions

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -34,6 +34,46 @@ if ( ! defined( 'WP_PLUGIN_DIR' ) ) {
 	define( 'WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins' );
 }
 
+// Mock WP_Error class
+if ( ! class_exists( 'WP_Error' ) ) {
+	class WP_Error {
+		public $errors = array();
+		public $error_data = array();
+
+		public function __construct( $code = '', $message = '', $data = '' ) {
+			if ( ! empty( $code ) ) {
+				$this->add( $code, $message, $data );
+			}
+		}
+
+		public function add( $code, $message, $data = '' ) {
+			$this->errors[ $code ][] = $message;
+			if ( ! empty( $data ) ) {
+				$this->error_data[ $code ] = $data;
+			}
+		}
+
+		public function get_error_code() {
+			$codes = array_keys( $this->errors );
+			return ! empty( $codes ) ? $codes[0] : '';
+		}
+
+		public function get_error_message( $code = '' ) {
+			if ( empty( $code ) ) {
+				$code = $this->get_error_code();
+			}
+			return isset( $this->errors[ $code ] ) ? $this->errors[ $code ][0] : '';
+		}
+
+		public function get_error_data( $code = '' ) {
+			if ( empty( $code ) ) {
+				$code = $this->get_error_code();
+			}
+			return isset( $this->error_data[ $code ] ) ? $this->error_data[ $code ] : '';
+		}
+	}
+}
+
 // Mock WordPress functions commonly used in plugins.
 if ( ! function_exists( '__' ) ) {
 	function __( $text, $domain = 'default' ) {
@@ -207,6 +247,23 @@ if ( ! class_exists( 'GFForms' ) ) {
 
 		public static function include_addon_framework() {}
 	}
+}
+
+// Define additional constants
+if ( ! defined( 'GF_VERIFYTX_VERSION' ) ) {
+	define( 'GF_VERIFYTX_VERSION', '0.1.0' );
+}
+
+if ( ! defined( 'GF_VERIFYTX_MIN_GF_VERSION' ) ) {
+	define( 'GF_VERIFYTX_MIN_GF_VERSION', '2.5' );
+}
+
+if ( ! defined( 'GF_VERIFYTX_PLUGIN_URL' ) ) {
+	define( 'GF_VERIFYTX_PLUGIN_URL', 'http://localhost/wp-content/plugins/gf-verifytx-addon/' );
+}
+
+if ( ! defined( 'GF_VERIFYTX_PLUGIN_BASENAME' ) ) {
+	define( 'GF_VERIFYTX_PLUGIN_BASENAME', 'gf-verifytx-addon/gf-verifytx-addon.php' );
 }
 
 // Load plugin files for testing.
